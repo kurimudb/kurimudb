cfb7a5b62b223ed96efc3166897b978be5a42772
akirarika <33272184+akirarika@users.noreply.github.com>
2024-10-18 10:29:57 +0800

    🎈 publish: v1.0.0-alpha.12

--- packages/milkio/use/index.ts
+++ packages/milkio/use/index.ts
@@ -1,14 +1,18 @@
-import type { $context } from "../context";
+import { $context } from "../context";
 
-export const defineUse = <CreatorFn extends (context: $context) => Promise<unknown> | unknown>(creatorFn: CreatorFn): ((context: $context) => Promise<Awaited<ReturnType<CreatorFn>>>) => {
-  let use: any | undefined;
+export const defineUse = <Mode extends "dynamic" | "static", Use extends Mode extends "static" ? () => Promise<unknown> | unknown : (...args: [$context, ...Array<unknown>]) => Promise<unknown> | unknown>(mode: Mode, use: Use): ((...args: Parameters<Use>) => Promise<Awaited<ReturnType<Use>>>) => {
+  if (mode === "static") {
+    let useCreated: any | undefined;
 
-  const getUse = async (context: $context) => {
-    if (use === undefined) {
-      use = await creatorFn(context);
-    }
-    return use;
-  };
+    const getUse = async () => {
+      if (useCreated === undefined) {
+        useCreated = await (use as any)();
+      }
+      return useCreated;
+    };
 
-  return getUse;
+    return getUse;
+  } else {
+    return use as any;
+  }
 };
