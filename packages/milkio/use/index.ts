2f11d19b7257f90a64bcd74696982ab231b766bf
akirarika <33272184+akirarika@users.noreply.github.com>
2024-10-18 10:03:52 +0800

    🎈 publish: v1.0.0-alpha.11

--- packages/milkio/use/index.ts
+++ packages/milkio/use/index.ts
@@ -1,9 +1,11 @@
-export const defineUse = <CreatorFn extends () => Promise<unknown> | unknown>(creatorFn: CreatorFn): (() => Promise<Awaited<ReturnType<CreatorFn>>>) => {
+import type { $context } from "../context";
+
+export const defineUse = <CreatorFn extends (context: $context) => Promise<unknown> | unknown>(creatorFn: CreatorFn): ((context: $context) => Promise<Awaited<ReturnType<CreatorFn>>>) => {
   let use: any | undefined;
 
-  const getUse = async () => {
+  const getUse = async (context: $context) => {
     if (use === undefined) {
-      use = await creatorFn();
+      use = await creatorFn(context);
     }
     return use;
   };
