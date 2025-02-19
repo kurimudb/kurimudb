1c7e85f06c387a4c713b20358cc418432fe12ee1
akirarika <33272184+akirarika@users.noreply.github.com>
2025-02-19 14:57:30 +0800

    ✨ feat: the `declares` function has been refactored, eliminating duplicate code and simplifying the logic of interface declarations. The original interface inheritance statements have been replaced with direct extensions

--- packages/cookbook/src/generator/declares.ts
+++ packages/cookbook/src/generator/declares.ts
@@ -1,12 +1,6 @@
-import { $, Glob } from 'bun'
-import consola from 'consola'
+import {  Glob } from 'bun'
 import { join } from 'node:path'
-import { exists, mkdir, readdir, readFile, unlink, writeFile } from 'node:fs/promises'
-import { exit } from 'node:process'
 import type { CookbookOptions } from '../utils/cookbook-dto-types'
-import { checkPath, toSafeImportName } from './utils'
-import { calcHash } from '../utils/calc-hash'
-import { progress } from '../progress'
 
 export async function declares(options: CookbookOptions, paths: { cwd: string, milkio: string, generated: string }, project: CookbookOptions['projects']['key']) {
     let declaresImports = '// declares'
@@ -25,11 +19,12 @@ export async function declares(options: CookbookOptions, paths: { cwd: string, m
      * ------------------------------------------------------------------------------------------------
      */
     const contextDts = (new Glob(`{contexts}/**/{*.d.ts`)).scan({ cwd: join(paths.cwd), onlyFiles: true })
-    declaresFile += `\n  interface $context extends `
+    declaresFile += `\n  interface $context`
     let contextIndex = 0
     for await (const path of contextDts) {
         declaresImports+= `\nimport type { _ as context_${contextIndex} } from "../${path.replaceAll('\\', '/')}";`
         if (contextIndex > 0) declaresFile += `, `
+        else declaresFile += ` extends `
         declaresFile += `context_${contextIndex}`
         ++contextIndex;
     }
@@ -41,11 +36,12 @@ export async function declares(options: CookbookOptions, paths: { cwd: string, m
      * ------------------------------------------------------------------------------------------------
      */
     const eventDts = (new Glob(`{events}/**/{*.d.ts`)).scan({ cwd: join(paths.cwd), onlyFiles: true })
-    declaresFile += `\n  interface $event extends `
+    declaresFile += `\n  interface $event`
     let eventIndex = 0
     for await (const path of eventDts) {
         declaresImports+= `\nimport type { _ as event_${eventIndex} } from "../${path.replaceAll('\\', '/')}";`
         if (eventIndex > 0) declaresFile += `, `
+        else declaresFile += ` extends `
         declaresFile += `event_${eventIndex}`
         ++eventIndex;
     }
@@ -57,11 +53,12 @@ export async function declares(options: CookbookOptions, paths: { cwd: string, m
      * ------------------------------------------------------------------------------------------------
      */
     const codeDts = (new Glob(`{codes}/**/{*.d.ts`)).scan({ cwd: join(paths.cwd), onlyFiles: true })
-    declaresFile += `\n  interface $rejectCode extends `
+    declaresFile += `\n  interface $rejectCode`
     let codeIndex = 0
     for await (const path of codeDts) {
         declaresImports+= `\nimport type { _ as code_${codeIndex} } from "../${path.replaceAll('\\', '/')}";`
         if (codeIndex > 0) declaresFile += `, `
+        else declaresFile += ` extends `
         declaresFile += `code_${codeIndex}`
         ++codeIndex;
     }
